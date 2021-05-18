3a8f3becbd2753cf6fd1009623fa39f61ab24270
MachikoFujii <machiko.fujii@gmail.com>
2021-05-18 14:46:53 +0900

    Fixed html.erb file names

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,27 @@
 class TasksController < ApplicationController
-  def _form,
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを作成しました！"
+    else
+      render :new 
+    end
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    if @task.update
+      redirect_to tasks_path, notice: "タスクをアップデートしました！"
+    else
+      render :new
+    end
   end
 
-  def edit,
-  end
-
-  def index,
-  end
-
-  def new,
+  def new
   end
 
   def show
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  get 'tasks/_form,'
-  get 'tasks/edit,'
-  get 'tasks/index,'
-  get 'tasks/new,'
-  get 'tasks/show'
   resources :tasks
 end

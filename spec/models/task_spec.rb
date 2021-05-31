6aa5e456eae228d733c73d625650a84a29da53f3
MachikoFujii <machiko.fujii@gmail.com>
2021-06-01 08:37:20 +0900

    test: wrote tests

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,20 +3,49 @@ RSpec.describe 'Task model', type: :model do
   describe 'validation test' do
     context 'when task name field is empty' do
       it 'violates presence validation' do
-        task = Task.new(task_name: '', task_description: 'desc1')
+        task = Task.new(name: '', description: 'desc1')
         expect(task).not_to be_valid
       end
     end
     context 'when task description is empty' do
       it 'violates presence vailidation' do
-        task = Task.new(task_name: 'task1', task_description: '')
+        task = Task.new(name: 'task1', description: '')
         expect(task).not_to be_valid
       end
     end
     context 'when task name and description fields are filled' do
       it 'goes through validation' do
-        task = Task.new(task_name: 'task1', task_description: 'desc1')
+        task = Task.new(name: 'task1', description: 'desc1')
       end
     end
   end
-end      
+
+  describe 'scope test' do
+    let!(:task) { FactoryBot.create(:task, name: 'task7', due_date: '2021-05-03', status: 2) }
+    let!(:task2) { FactoryBot.create(:task2, name: 'task5', due_date: '2021-06-09', status: 1) }
+    context "when fuzzy search performed by task name" do
+      it "shows result(s) includes the keyword" do
+        expect(Task.search_name(7)).to include(task)
+        expect(Task.search_name(7)).not_to include(task2 )
+        expect(Task.search_name(7).count).to eq 1
+      end
+    end
+    context 'when searched by status' do
+      it 'shows the matching result' do
+        expect(Task.search_status('完了')).to include(task)
+        expect(Task.search_status('完了')).not_to include(task2)
+        expect(Task.search_status('完了').count).to eq 1
+      end
+    end
+    context 'when fuzzy searched by task name and status' do
+      it 'shows the matching result(s)' do   
+        expect(Task.search_name(7)).to include(task)
+        expect(Task.search_name(7)).not_to include(task2)
+        expect(Task.search_name(7).count).to eq 1
+        expect(Task.search_status('完了')).to include(task)
+        expect(Task.search_status('完了')).not_to include(task2)
+        expect(Task.search_status('完了').count).to eq 1 
+      end
+    end
+  end
+end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -9,52 +9,77 @@ describe 'New task' do
       fill_in :task_name, with: 'task1'
       fill_in :task_description, with: 'desc1'
       click_button 'タスクを作成'
-      expect(page).to have_text ('task1')
-      expect(page).to have_text ('desc1')
+      expect(page).to have_content 'task1'
+      expect(page).to have_content 'desc1'
     end
   end
 end
   context "when the link 期限でソート is clicked" do
     it "sorts the task list by deadline in ascending order" do
+      task = FactoryBot.create(:task)
+      task2 = FactoryBot.create(:task2)
+      task3 = FactoryBot.create(:task3)
       visit tasks_path
       click_on '期限でソート'
-      task_list = all('.task_row')
-      expect(task_list[0]).to have_text tasks.first.name
+      task_list = all('.deadline')
+      sleep 0.3
+      # expect(task_list[0]).to have_content "task7"
+      # expect(task_list[1]).to have_content "task1"
+      # expect(task_list[2]).to have_content "task5"
+      expect(task_list[0]).to have_content "2021-05-03"
+      expect(task_list[1]).to have_content "2021-06-01"
+      expect(task_list[2]).to have_content "2021-06-09"
     end
   end
-# describe 'Index page' do
-#   context 'when an user visits index page' do
-#     it 'shows existing tasks' do
-#       task = FactoryBot.create(:task)
-#       task2 = FactoryBot.create(:task2)
-#       task3 = FactoryBot.create(:task3)
-#       visit tasks_path
-#       expect(page).to have_text ('task1')
-#       expect(page).to have_text ('task2')
-#       expect(page).to have_text ('task3')
-#     end
-#   end
-# end
-# describe 'Show page' do
-#   context 'when an user visits a task description page' do
-#     it 'shows task description' do
-#       task = FactoryBot.create(:task)
-#       visit tasks_path
-#       expect(page).to have_text ('task1')
-#     end
-#   end
-# end
-# describe 'Index page' do
-#   context 'when tasks are listed in descending order' do
-#     it 'shows the newest task on top' do
-#       task = FactoryBot.create(:task)
-#       task2 = FactoryBot.create(:task2)
-#       task3 = FactoryBot.create(:task3)
-#       visit tasks_path
-#       task_list = all('.task')
-#       expect(task_list[0]).to have_text ('task3')
-#     end
-#     end
-#   end
+end
+describe 'Index page' do
+  context 'when an user visits index page' do
+    it 'shows existing tasks' do
+      task = FactoryBot.create(:task)
+      task2 = FactoryBot.create(:task2)
+      task3 = FactoryBot.create(:task3)
+      visit tasks_path
+      expect(page).to have_text ('task7')
+      expect(page).to have_text ('task5')
+      expect(page).to have_text ('task1')
+    end
+  end
+  context 'when fuzzy searched by task name' do
+    it 'shows matching result' do
+      task = FactoryBot.create(:task)
+      visit tasks_path
+      fill_in :search_name, with: '7'
+      click_button '検索'
+      expect(page).to have_content 'task7'
+    end
+  end
+  context 'when searched by status' do
+    it 'shows matching result' do
+      task = FactoryBot.create(:task)
+      visit tasks_path
+      select '着手中', from: "status"
+      click_button '検索'
+      expect(page).to have_content '着手中'
+    end
+  end
+  context 'when fuzzy searched by task name and status' do
+    it 'shows matching result' do
+      task = FactoryBot.create(:task)
+      visit tasks_path
+      fill_in :search_name_status, with: '7'
+      select '着手中', from: "status"
+      click_button '検索'
+      expect(page).to have_content 'task7'
+    end
+  end
+end
+describe 'Show page' do
+  context 'when an user visits a task description page' do
+    it 'shows task description' do
+      task = FactoryBot.create(:task)
+      visit tasks_path
+      expect(page).to have_text ('task7')
+    end
+  end
 end
 

2f44b92ffa8aa98568334d3fa930b6ee4aa532eb
MachikoFujii <machiko.fujii@gmail.com>
2021-06-01 16:07:21 +0900

    DB: cerated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,18 @@
-100.times do |i|
-  Task.create!(
-    name: "task#{i}",
-    description: "desc#{i}"
-  )
+require 'i18n'
+I18n.locale # => :en
+I18n.locale = :ja
+I18n.locale # => :ja
+require 'faker'
+Faker::Config.locale # => :ja
+Faker::Internet.email # => ".@.com"
+
+
+10.times do |i|
+  name = Faker::Games::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password_digest: password,
+               )
 end

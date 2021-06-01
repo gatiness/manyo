be5b64855e97786dd0cbb144a3e0bdae37913cac
MachikoFujii <machiko.fujii@gmail.com>
2021-06-01 16:52:10 +0900

    DB: implemented unique constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_054139) do
+ActiveRecord::Schema.define(version: 2021_06_01_074842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_054139) do
     t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end

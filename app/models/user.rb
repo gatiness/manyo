4d3ce46d904b5ce7df9ee40a81a14ea478a8a1f8
MachikoFujii <machiko.fujii@gmail.com>
2021-06-02 16:50:14 +0900

    Feat: created log-in form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_many :tasks
-
+  
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+ 
   before_validation { email.downcase! }
-
   validates :email, uniqueness: true
   validates :name, presence: true, length: { maximum: 30 } 
   validates :email, presence: true, length: { maximum: 255 }, 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/_form'
-  root "tasks#index"
-  resources :tasks
+  resources :users, only: [:new, :create]
 end
